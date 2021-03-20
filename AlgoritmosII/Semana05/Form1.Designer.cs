4e4c32f48000caa5d41cd3cc783389f31098321c
Roy <roy.rojval@gmail.com>
2021-03-20 13:25:26 -0500

    semana 05

--- AlgoritmosII/Semana05/Form1.Designer.cs
+++ AlgoritmosII/Semana05/Form1.Designer.cs
@@ -38,7 +38,8 @@ namespace Semana05
             this.txtResultado = new System.Windows.Forms.TextBox();
             this.btnMayorMenor = new System.Windows.Forms.Button();
             this.btnSumatoriaFila = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSumaColumna3 = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -75,7 +76,7 @@ namespace Semana05
             // 
             // btnGenerar
             // 
-            this.btnGenerar.Location = new System.Drawing.Point(41, 134);
+            this.btnGenerar.Location = new System.Drawing.Point(41, 113);
             this.btnGenerar.Name = "btnGenerar";
             this.btnGenerar.Size = new System.Drawing.Size(198, 23);
             this.btnGenerar.TabIndex = 4;
@@ -85,54 +86,68 @@ namespace Semana05
             // 
             // btnListarSuma
             // 
-            this.btnListarSuma.Location = new System.Drawing.Point(41, 189);
+            this.btnListarSuma.Location = new System.Drawing.Point(41, 225);
             this.btnListarSuma.Name = "btnListarSuma";
             this.btnListarSuma.Size = new System.Drawing.Size(198, 23);
             this.btnListarSuma.TabIndex = 5;
             this.btnListarSuma.Text = "Sumatoria y Promedio";
             this.btnListarSuma.UseVisualStyleBackColor = true;
+            this.btnListarSuma.Click += new System.EventHandler(this.btnListarSuma_Click);
             // 
             // txtResultado
             // 
             this.txtResultado.Location = new System.Drawing.Point(266, 15);
             this.txtResultado.Multiline = true;
             this.txtResultado.Name = "txtResultado";
-            this.txtResultado.Size = new System.Drawing.Size(378, 408);
+            this.txtResultado.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResultado.Size = new System.Drawing.Size(753, 408);
             this.txtResultado.TabIndex = 6;
             // 
             // btnMayorMenor
             // 
-            this.btnMayorMenor.Location = new System.Drawing.Point(41, 253);
+            this.btnMayorMenor.Location = new System.Drawing.Point(41, 278);
             this.btnMayorMenor.Name = "btnMayorMenor";
             this.btnMayorMenor.Size = new System.Drawing.Size(198, 23);
             this.btnMayorMenor.TabIndex = 7;
             this.btnMayorMenor.Text = "#Mayor , #Menor";
             this.btnMayorMenor.UseVisualStyleBackColor = true;
+            this.btnMayorMenor.Click += new System.EventHandler(this.btnMayorMenor_Click);
             // 
             // btnSumatoriaFila
             // 
-            this.btnSumatoriaFila.Location = new System.Drawing.Point(41, 315);
+            this.btnSumatoriaFila.Location = new System.Drawing.Point(41, 329);
             this.btnSumatoriaFila.Name = "btnSumatoriaFila";
             this.btnSumatoriaFila.Size = new System.Drawing.Size(198, 23);
             this.btnSumatoriaFila.TabIndex = 8;
             this.btnSumatoriaFila.Text = "Sumatoria Fila 2";
             this.btnSumatoriaFila.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnSumaColumna3
             // 
-            this.button1.Location = new System.Drawing.Point(41, 381);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(198, 23);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Sumatoria Columna 3";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSumaColumna3.Location = new System.Drawing.Point(41, 381);
+            this.btnSumaColumna3.Name = "btnSumaColumna3";
+            this.btnSumaColumna3.Size = new System.Drawing.Size(198, 23);
+            this.btnSumaColumna3.TabIndex = 9;
+            this.btnSumaColumna3.Text = "Sumatoria Columna 3";
+            this.btnSumaColumna3.UseVisualStyleBackColor = true;
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(41, 166);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(198, 23);
+            this.btnBorrar.TabIndex = 10;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(656, 443);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1056, 443);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnSumaColumna3);
             this.Controls.Add(this.btnSumatoriaFila);
             this.Controls.Add(this.btnMayorMenor);
             this.Controls.Add(this.txtResultado);
@@ -161,7 +176,8 @@ namespace Semana05
         private System.Windows.Forms.TextBox txtResultado;
         private System.Windows.Forms.Button btnMayorMenor;
         private System.Windows.Forms.Button btnSumatoriaFila;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSumaColumna3;
+        private System.Windows.Forms.Button btnBorrar;
     }
 }
 
--- AlgoritmosII/Semana05/Form1.cs
+++ AlgoritmosII/Semana05/Form1.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Semana05
 {
     public partial class Form1 : Form
     {
-        int[][] numeros;
+        int[,] numeros;
+        int filas, columnas;
 
         public Form1()
         {
@@ -21,13 +15,91 @@ namespace Semana05
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
         private void btnGenerar_Click(object sender, EventArgs e)
         {
-          
+            filas = int.Parse(txtFila.Text);
+            columnas = int.Parse(txtColumna.Text);
+            //inicializar mi arreglo de numeros
+            numeros = new int[filas, columnas];
+            generarNumeros(filas, columnas);
+            //mostrar los datos
+            string mensaje = obtenerNumeros();
+            txtResultado.Text = mensaje;
+        }
 
+        private string obtenerNumeros()
+        {
+            string mensaje = "";
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    mensaje += "n[" + i + "," + j + "] = " + numeros[i, j] + "\t";
+                }
+                mensaje += Environment.NewLine;
+            }
+            return mensaje;
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            numeros = null;
+            MessageBox.Show("arreglo limpiado");
+            txtResultado.Text = "";
+        }
+
+        private void btnListarSuma_Click(object sender, EventArgs e)
+        {
+            int suma = 0;
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    suma += numeros[i, j];
+                }
+            }
+            string mensaje = obtenerNumeros();
+            mensaje += "La suma es :" + suma;
+            mensaje += Environment.NewLine;
+            double promedio = suma * 1.0 / numeros.Length;
+            mensaje += "El promedio es : " + promedio;
+
+            txtResultado.Text = mensaje;
+        }
+
+        private void btnMayorMenor_Click(object sender, EventArgs e)
+        {
+            int mayor = 0; int menor = 100;
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    if (numeros[i, j] < menor) menor = numeros[i, j];
+                    if (numeros[i, j] > mayor) mayor = numeros[i, j];
+                }
+            }
+
+            string mensaje = obtenerNumeros();
+            mensaje += "El numero menor  es :" + menor;
+            mensaje += Environment.NewLine;
+            mensaje += "El numero mayor  es :" + mayor;
+            txtResultado.Text = mensaje;
+        }
+
+        private void generarNumeros(int filas, int columnas)
+        {
+            Random random = new Random();
+            //recorriendo las filas
+            for (int i = 0; i < filas; i++)
+            {
+                //recorriendo las columnas
+                for (int j = 0; j < columnas; j++)
+                {
+                    numeros[i, j] = random.Next(10, 99);
+                }
+            }
         }
     }
 }
