acebdd377e012db432ede67059d292cc882734e4
Roy <roy.rojval@gmail.com>
2021-04-06 19:32:37 -0500

    agregar semana 10

---
+++ AlgoritmosII/Semana10/FormMatematica.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace Semana10
+{
+    partial class FormMatematica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtParcial = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPractica3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPractica2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPractica1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(202, 286);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(99, 23);
+            this.btnCancelar.TabIndex = 27;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(75, 286);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(99, 23);
+            this.btnProcesar.TabIndex = 26;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(201, 342);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(100, 20);
+            this.txtPromedio.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(72, 345);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Promedio";
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Location = new System.Drawing.Point(201, 228);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(100, 20);
+            this.txtFinal.TabIndex = 23;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(72, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Final";
+            // 
+            // txtParcial
+            // 
+            this.txtParcial.Location = new System.Drawing.Point(201, 181);
+            this.txtParcial.Name = "txtParcial";
+            this.txtParcial.Size = new System.Drawing.Size(100, 20);
+            this.txtParcial.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Parcial";
+            // 
+            // txtPractica3
+            // 
+            this.txtPractica3.Location = new System.Drawing.Point(201, 130);
+            this.txtPractica3.Name = "txtPractica3";
+            this.txtPractica3.Size = new System.Drawing.Size(100, 20);
+            this.txtPractica3.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Practica 3";
+            // 
+            // txtPractica2
+            // 
+            this.txtPractica2.Location = new System.Drawing.Point(201, 78);
+            this.txtPractica2.Name = "txtPractica2";
+            this.txtPractica2.Size = new System.Drawing.Size(100, 20);
+            this.txtPractica2.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Practica 2";
+            // 
+            // txtPractica1
+            // 
+            this.txtPractica1.Location = new System.Drawing.Point(201, 35);
+            this.txtPractica1.Name = "txtPractica1";
+            this.txtPractica1.Size = new System.Drawing.Size(100, 20);
+            this.txtPractica1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Practica 1";
+            // 
+            // FormMatematica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 397);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.txtPromedio);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtParcial);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPractica3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPractica2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPractica1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMatematica";
+            this.Text = "FormMatematica";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMatematica_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtParcial;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPractica3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPractica2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPractica1;
+        private System.Windows.Forms.Label label1;
+    }
+}
