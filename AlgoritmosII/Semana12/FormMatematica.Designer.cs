1064c3869b542c730c87d824d2158a2cebfdf93b
Roy <roy.rojval@gmail.com>
2021-04-14 22:40:39 -0500

    semana 12

---
+++ AlgoritmosII/Semana12/FormMatematica.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Semana12
+{
+    partial class FormMatematica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtParcial = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrueba = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(204, 207);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(99, 23);
+            this.btnCancelar.TabIndex = 51;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(77, 207);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(99, 23);
+            this.btnProcesar.TabIndex = 50;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(203, 263);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(100, 20);
+            this.txtPromedio.TabIndex = 49;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(74, 266);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 48;
+            this.label6.Text = "Promedio";
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Location = new System.Drawing.Point(203, 146);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(100, 20);
+            this.txtFinal.TabIndex = 47;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(74, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 46;
+            this.label5.Text = "Final";
+            // 
+            // txtParcial
+            // 
+            this.txtParcial.Location = new System.Drawing.Point(203, 99);
+            this.txtParcial.Name = "txtParcial";
+            this.txtParcial.Size = new System.Drawing.Size(100, 20);
+            this.txtParcial.TabIndex = 45;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 44;
+            this.label4.Text = "Parcial";
+            // 
+            // txtPrueba
+            // 
+            this.txtPrueba.Location = new System.Drawing.Point(204, 45);
+            this.txtPrueba.Name = "txtPrueba";
+            this.txtPrueba.Size = new System.Drawing.Size(100, 20);
+            this.txtPrueba.TabIndex = 43;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Prueba";
+            // 
+            // FormMatematica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 336);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.txtPromedio);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtParcial);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPrueba);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMatematica";
+            this.Text = "FormMatematica";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMatematica_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtParcial;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPrueba;
+        private System.Windows.Forms.Label label1;
+    }
+}
