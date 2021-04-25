d5938aea2a058103be5b7ff56e4514c853bc7257
Roy <roy.rojval@gmail.com>
2021-04-24 20:49:18 -0500

    Revert "Semana 13"
    This reverts commit e4756ce7f368fbb7cbf0af277bf40af3572930d1.

--- AlgoritmosII/Semana13/FormTrabajadoEstable.cs
+++ AlgoritmosII/Semana13/FormTrabajadoEstable.cs
@@ -24,21 +24,7 @@ namespace Semana13
 
         private void btnProcesar_Click(object sender, EventArgs e)
         {
-            try
-            {
-                //leer datos
-                double sueldoBase = double.Parse(txtSueldoBruto.Text);
-                //Trabajador trabajador = new Trabajador(""); 
-                //NO SE PUEDE CREAR UNA INSTANCIA DE LA CLASE ABSTRACTA
-                TrabajadorPlanilla trabajadorPlanilla = new TrabajadorPlanilla(sueldoBase);
-                //mostrar datos
-                string mensaje = trabajadorPlanilla.CalcularPago();
-                txtResultado.Text = mensaje;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ocurrio un error, intentelo más tarde");
-            }
+
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
